cff0d2a0e348a99e4c2d3837b95b67da7910fd38
pauline.clausse65@gmail.com <pauline.clausse65@gmail.com>
2026-01-29 11:19:00 +0100

    Ajout de l'adaptateur broker/BDD

---
+++ receiver/src/adapter/broker.js
@@ -0,0 +1,26 @@
+const mosquitto = require('mqtt');
+
+class Broker {
+  constructor(brokerUrl, options = {}) {
+    this.client = mosquitto.connect(brokerUrl, options);
+    this.subscriptions = new Map();
+
+    this.client.on('message', (topic, message) => {
+        if (this.subscriptions.has(topic)) {
+            this.subscriptions.get(topic).forEach(callback => callback(message));
+        }
+    });
+  }
+   subscribe(topic, callback) {
+    if (!this.subscriptions.has(topic)) {
+        this.subscriptions.set(topic, []);
+        this.client.subscribe(topic);
+    }
+    this.subscriptions.get(topic).push(callback);
+  }
+    publish(topic, message) {
+    this.client.publish(topic, message);
+    }
+}
+
+module.exports = Broker;
